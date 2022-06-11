6af37c04ee9d4d10ecf4d9784cfd8ccc8bf9bcb2
Gisselt <gissotk09>
2022-06-10 22:42:04 -0500

    creada maqueta de banner y filtro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TitleBannerComponent } from './components/title-banner/title-banner.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { FilterComponent } from './components/filter/filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleBannerComponent,
+    InicioComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
