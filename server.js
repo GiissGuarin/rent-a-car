d254242666c316ddeb80f1a2cd1ed73978e4fd52
Gisselt <gissotk09>
2022-06-13 08:55:30 -0500

    configuraciones para heroku

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
+app.use(express.static(__dirname+'/dist/rent-a-car'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/rent-a-car/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
